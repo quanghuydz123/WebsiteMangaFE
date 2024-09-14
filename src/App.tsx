3eaeac326f87040a076d779812a14a2552d9cee3
Huy <dinhphongtamquoc453@gmail.com>
2024-09-14 18:12:26 +0700

    config connect server nodejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import userAPI from './apis/userApi';
 
 function App() {
+  const handleCallApi = async ()=>{
+    const api = '/get-all'
+    try {
+      const res = await userAPI.HandleUser(api)
+      console.log("res",res)
+    } catch (error:any) {
+      const errorMessage = JSON.parse(error.message)
+      console.log("HomeScreen", errorMessage)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=>handleCallApi()}>click</button>
     </div>
   );
 }
---
+++ src/constrants/webInfo.ts
@@ -0,0 +1,4 @@
+
+export const appInfo = {
+    BASE_URL: 'http://localhost:3001'
+}

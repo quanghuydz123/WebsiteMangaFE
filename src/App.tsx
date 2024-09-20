7358d1df3f47f214f4ef75f1fa58652e748f6a7c
dtoan <dtoan1612@gmail.com>
2024-09-20 23:40:37 +0700

    fix: fix route in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomeAdminPage from './pages/admin/Home/HomeAdminPage';
 import HomeUserPage from './pages/user/HomeUserPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomeUserPage />} />
-        <Route path='/admin' element={<HomeAdminPage />} />
-        {/* Add a 404 page */}
-        <Route path='*' element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomeUserPage />} />
+      <Route path='/admin' element={<HomeAdminPage />} />
+      {/* Thêm một trang 404 */}
+      <Route path='*' element={<h1>404 - Page Not Found</h1>} />
+    </Routes>
   );
 }
 

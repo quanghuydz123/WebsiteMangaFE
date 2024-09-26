978efc787a2eb6dc4d7473ce86afcc46f9d0a46b
Tuavsn <hoctuavsn@gmail.com>
2024-09-26 03:50:09 +0000

    config header

--- src/components/User/Header/Header.tsx
+++ src/components/User/Header/Header.tsx
@@ -4,39 +4,14 @@ import Logo from "../../../assets/images/logo.png"
 
 export default function Header() {
     return (
-        <div className="container flex justify-between items-center mx-2 md:mx-auto pt-6 ">
+        <div className="container flex gap-6 justify-between items-center mx-4 md:mx-auto pt-6 ">
             {/* Logo */}
             <div>
                 <img className="w-40 rounded-lg" src={Logo} />
             </div>
-            <div className="grow flex justify-evenly">
+            <div className="grow flex justify-end">
             {/* Navigation Bar */}
                 <NavigationBar />
-            {/* Search Bar */}
-                <SearchBar />
-            </div>
-            {/* Login Button */}
-            <div>
-                <button 
-                type="button" 
-                className="
-                    hidden lg:block
-                    border-2
-                    border-slate-500
-                    bg-gray-800
-                    hover:bg-gray-900 
-                    focus:outline-none 
-                    focus:ring-4 
-                    focus:ring-gray-300 
-                    font-medium 
-                    rounded-lg 
-                    px-5 py-2.5
-                    dark:bg-gray-800 
-                    dark:hover:bg-gray-700 
-                    dark:focus:ring-gray-700 
-                    dark:border-gray-700">
-                    Đăng nhập
-                </button>
             </div>
         </div>
     )

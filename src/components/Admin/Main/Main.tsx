3a2bcf0568650bc32ec0b7a60d57e0ab743cb3fd
dtoan <dtoan1612@gmail.com>
2024-10-18 22:35:45 +0700

    update User, Manga management template

--- src/components/Admin/Main/Main.tsx
+++ src/components/Admin/Main/Main.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header';
-import Card from '../Cards/Card';
-import CustomBarChart from '../Charts/CustomBarChart';
-import CustomPieChart from '../Charts/CustomPieChart';
-import Table from '../Table/Table';
-import Activity from '../Activity/Activity';
-import {motion} from "framer-motion";
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route,  Routes } from 'react-router-dom';
 import Analysis from '../../../pages/admin/Analysis/Analysis';
 import User from '../../../pages/admin/User/User';
 import Manga from '../../../pages/admin/Manga/Manga';
@@ -18,7 +12,7 @@ import Chapter from '../../../pages/admin/Chapter/Chapter';
 const MainContent = ({ isOpen, toggleDarkMode, darkMode }: {isOpen: boolean, toggleDarkMode: () => void, darkMode:boolean}) => {
   return (
     <div
-        className={`flex-1 bg-slate-200 ${
+        className={`min-h-screen flex-1 bg-slate-200 ${
           isOpen ? "md:ml-44" : "ml-16"
         } transition-all duration-300 dark:bg-slate-800`}
     >
